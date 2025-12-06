c29ad947cd66bfb16e2c232b693525c3ecac56e9
yuanzhixiang <i@yuanzhixiang.com>
2025-12-07 01:07:08 +0800

    modify dev

--- hello/config.js
+++ hello/config.js
@@ -2,6 +2,7 @@ var path = require("path");
 
 module.exports = {
   entry: "./input",
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "../build/oss-experimental/hello"),
     filename: "index.js",
