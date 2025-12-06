f002ef1c83e687827a89a2b22e817e251d9bc48c
yuanzhixiang <i@yuanzhixiang.com>
2025-12-07 00:36:42 +0800

    add webpack dev project

---
+++ fixtures/packaging/webpack/dev/input.js
@@ -0,0 +1,12 @@
+// var React = require('react');
+// // React 18+ 的挂载入口在 react-dom/client
+// var ReactDOMClient = require('react-dom/client');
+
+// var container = document.getElementById('container');
+// var root = ReactDOMClient.createRoot(container);
+
+// root.render(React.createElement('h1', null, 'Hello World!'));
+
+console.log("hello");
+
+require("hello");
