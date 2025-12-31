e089c277460a3514cf67f4ca231de2499cb0bc2f
yuanzhixiang <i@yuanzhixiang.com>
2025-12-31 08:12:47 +0800

    feat: implement createElement function and update ReactClient structure

--- fixtures/packaging/webpack/dev/input.js
+++ fixtures/packaging/webpack/dev/input.js
@@ -2,9 +2,9 @@
 // // React 18+ 的挂载入口在 react-dom/client
 // var ReactDOMClient = require('react-dom/client');
 
-import { createRoot } from "react-dom/client";
+import * as React from "react";
+import * as ReactDOMClient from "react-dom/client";
 
 var container = document.getElementById("container");
-var root = createRoot(container);
-
-// root.render(React.createElement('h1', null, 'Hello World!'));
+var root = ReactDOMClient.createRoot(container);
+root.render(React.createElement("h1", null, "Hello World!"));
---
+++ packages/react/src/jsx/ReactJSXElement.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Create and return a new ReactElement of the given type.
+ * See https://reactjs.org/docs/react-api.html#createelement
+ */
+export function createElement(type, config, children) {
+  throw new Error("Not implemented");
+}
